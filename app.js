238957604c3106c9aa85db681e304531634d1ab1
monupareeklg <monupareeklg@gmail.com>
2021-03-11 15:36:58 +0530

    Initilized Commit

---
+++ app.js
@@ -0,0 +1,102 @@
+require("dotenv").config();
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express()
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(session({
+    secret: "ourlittlesecret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.MONGO_PASS, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+const Post = mongoose.model("Post", userSchema)
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get('/', function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login")
+});
+
+app.get("/register", function(req, res) {
+    res.render("register")
+});
+
+app.post("/register", function (req, res) {
+    User.register({ username: req.body.username }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/")
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/chat")
+            });
+        }
+    });
+});
+
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.logIn(user, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.render("/")
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/chat")
+            });
+        }
+    });
+});
+
+app.get("/logout", function (req, res) {
+    req.logOut();
+    res.redirect("/");
+});
+
+app.get("/chat", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("chat");
+    } else {
+        res.redirect("/")
+    }
+})
+
+app.listen(3000, function () {
+    console.log("Server Started");
+});
